bce5edd088569bf223fe81a6e31d34fde62b1a6d
Swapna-321 <gowdaperuswapna@gmail.com>
2024-08-26 19:32:16 +0530

    datelogic

--- LocalDateExp.java
+++ LocalDateExp.java
@@ -1,4 +1,18 @@
 public class LocalDateExp{
 // Create a method addDaysToCurrentDate(int days) that adds a specified number of days to the current date and returns the new date.
   // Create a method getFirstDayOfMonth(LocalDate date) that returns the first day of the month for a given date.
+  public LocalDate addDaysToCurrentDate(int days){
+    LocalDate currentDate = LocalDate.now();
+    return currentDate.plusDays(days);
+  }
+  public LocalDate getFirstDayOfMonth(LocalDate date){
+   return LocalDate.of(date.getYear(), date.getMonth(), 1); 
+  }
+  public static void main(String args[]){
+    LocalDateExp lDate = new LocalDateExp();
+    LocalDate newDate=lDate.addDaysToCurrentDate(8);
+    System.out.println("Date after adding 8 days: " + newDate);
+    LocalDate firstDayOfMonth=lDate.getFirstDayOfMonth(LocalDate.now());
+    System.out.println("First day of the current month: " + firstDayOfMonth);
+  }
 }
