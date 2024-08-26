5ac610664fd7b60a1a548f5d3153378c7d91677e
Ro-045 <pratapnaidu016@gmail.com>
2024-08-26 20:06:27 +0530

    pratap

--- LocalDateExp.java
+++ LocalDateExp.java
@@ -1,4 +1,23 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 public class LocalDateExp{
 // Create a method addDaysToCurrentDate(int days) that adds a specified number of days to the current date and returns the new date.
   // Create a method getFirstDayOfMonth(LocalDate date) that returns the first day of the month for a given date.
+   static LocalDate addDaysToCurrentDate(int days){
+      LocalDate today = LocalDate.now();
+      LocalDate newDate = today.plusDays(days);
+      return newDate;
+    }
+
+   static DayOfWeek getFirstDayOfMonth(LocalDate date){
+      LocalDate firstDayOfMonth=date.withDayOfMonth(1);
+      return firstDayOfMonth.getDayOfWeek();
+
+    }
+
+    public static void main(String[] args) {
+      System.out.println(addDaysToCurrentDate(7));
+      LocalDate date= LocalDate.of(2023,11,25);
+      System.out.println(getFirstDayOfMonth(date));
+    }
 }
