9f414ff47361e696860f4cce794c7e8114e1fca6
kashifakibria <kashifakibria@gmail.com>
2024-08-26 20:17:19 +0530

    Date_Time

--- LocalDateTimeExp.java
+++ LocalDateTimeExp.java
@@ -1,4 +1,30 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class LocalDateTimeExp{
 // Write a method getCurrentDateTime() that returns the current date and time as a LocalDateTime object. Also, print the date-time in the format yyyy-MM-dd HH:mm:ss.
   //  Write a method isDateTimeBefore(LocalDateTime dateTime1, LocalDateTime dateTime2) that takes two LocalDateTime objects and returns true if dateTime1 is before dateTime2, otherwise returns false.
+public LocalDateTime getCurrenDateTime(){
+    LocalDateTime dateTime = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    String formattedDateTime = dateTime.format(formatter);
+    System.out.println(formattedDateTime);
+    return dateTime;
+
+}
+public boolean isDateTimeBefore(LocalDateTime dateTime1){
+    return dateTime1.isBefore(dateTime1);
+
+}
+public static void main(String[] args) {
+    LocalDateTimeExp exp = new LocalDateTimeExp();  
+    
+    LocalDateTime DateTime = exp.getCurrenDateTime();
+
+    boolean isBefore=exp.isDateTimeBefore(DateTime);
+    System.out.println(isBefore);
+
+
+    
+}
 }
--- LocalTimeExp.java
+++ LocalTimeExp.java
@@ -1,4 +1,35 @@
+import java.time.LocalTime;
+
 public class LocalTimeExp{
 // : Write a method addHoursToTime(LocalTime time, int hours) that takes a LocalTime object and an integer representing hours, and returns a new LocalTime with the specified number of hours added.
 // Write a method compareTimes(LocalTime time1, LocalTime time2) that compares two LocalTime objects and returns:
+
+public LocalTime addHoursToTime(LocalTime time, int hours){
+    return time.plusHours(hours);
+}
+public int compareTimes(LocalTime time1, LocalTime time2){
+    return time1.compareTo(time2);
+}
+public static void main(String[] args) {
+    LocalTimeExp exp = new LocalTimeExp();
+    LocalTime time1 = LocalTime.of(2, 30, 0);
+    LocalTime time2 = LocalTime.of(3, 30, 0);
+    LocalTime time3 = exp.addHoursToTime(time1, 5);
+    int result = exp.compareTimes(time1, time2);
+    System.out.println(time1);
+    System.out.println(time2);
+    System.out.println(time3);
+    System.out.println(result);
+
+    if(result == 0){
+        System.out.println("The times are equal");
+    }
+    else if(result < 0){
+        System.out.println("Time1 is earlier than Time2");
+    }
+    else{
+        System.out.println("Time1 is later than Time2");
+    }
+    
+}
 }
