0e59d03f9be4f51413ceabab0435db708fcdcb73
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-26 17:59:25 +0530

    date logics

---
+++ LocalDate.java
@@ -0,0 +1,9 @@
+
+public class LocalDate {
+
+    public LocalDate plusDays(int days) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'plusDays'");
+    }
+
+}
--- LocalTimeExp.java
+++ LocalTimeExp.java
@@ -1,4 +1,32 @@
+import java.time.LocalTime;
 public class LocalTimeExp{
+    public LocalTime addHoursToTime(LocalTime time, int hours){
+        return time.plusHours(hours);
+    }
+    public int compareTimes(LocalTime time1, LocalTime time2){
+        return time1.compareTo(time2);
+    }
+    public static void main(String[] args) {
+        LocalTimeExp lte = new LocalTimeExp();
+        LocalTime time1 = LocalTime.of( 12, 30, 0);
+        LocalTime time2 = LocalTime.of(14, 30, 0);
+        LocalTime time3 = lte.addHoursToTime(time1, 7);
+        System.out.println("added time: " + time3);
+        int Result = lte.compareTimes(time1, time2);
+        System.out.println("Time1: " + time1);
+        System.out.println("Time2: " + time2);
+        if(Result < 0){
+            System.out.println("Time1 is before Time2");
+        }
+        else if(Result > 0){
+            System.out.println("Time1 is after Time2");
+        }
+        else{
+            System.out.println("Time1 is equal to Time2");
+        }
+        System.out.println("Comparison result: " + Result);
+        
+    }
 // : Write a method addHoursToTime(LocalTime time, int hours) that takes a LocalTime object and an integer representing hours, and returns a new LocalTime with the specified number of hours added.
 // Write a method compareTimes(LocalTime time1, LocalTime time2) that compares two LocalTime objects and returns:
 }
