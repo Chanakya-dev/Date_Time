1a360265b3478d57d30ca22faebf2a4cd92b7d4f
psvsai <pasupuletisvsm245@gmail.com>
2024-08-26 20:20:39 +0530

    PSVSAI

--- LocalDateExp.java
+++ LocalDateExp.java
@@ -1,4 +1,24 @@
+import java.time.LocalDate;
+
 public class LocalDateExp{
 // Create a method addDaysToCurrentDate(int days) that adds a specified number of days to the current date and returns the new date.
   // Create a method getFirstDayOfMonth(LocalDate date) that returns the first day of the month for a given date.
+
+public LocalDate addDaysToCurrentDate(int days){
+  LocalDate currentDate = LocalDate.now();
+  return currentDate.plusDays(days);
+
+}
+public LocalDate getFirstDayOfMonth(LocalDate date){
+  LocalDate FirstDayOfMonth = date.withDayOfMonth(2);
+  return FirstDayOfMonth;
+
+}
+public static void main(String[] args) {
+  LocalDateExp Date = new LocalDateExp();
+  LocalDate newDate = Date.addDaysToCurrentDate(6);
+  System.out.println("Date afte ^ days is  : " + newDate);
+  LocalDate firstDayOfMonth = Date.getFirstDayOfMonth(LocalDate.of(2024, 3, 24));
+  System.out.println("First day of month :" + firstDayOfMonth);
+}
 }
--- LocalDateTimeExp.java
+++ LocalDateTimeExp.java
@@ -1,4 +1,28 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class LocalDateTimeExp{
-// Write a method getCurrentDateTime() that returns the current date and time as a LocalDateTime object. Also, print the date-time in the format yyyy-MM-dd HH:mm:ss.
-  //  Write a method isDateTimeBefore(LocalDateTime dateTime1, LocalDateTime dateTime2) that takes two LocalDateTime objects and returns true if dateTime1 is before dateTime2, otherwise returns false.
-}
+// Write a method getCurrentDateTime() that returns the current date and time as a LocalDateTime object. 
+// Also, print the date-time in the format yyyy-MM-dd HH:mm:ss.
+  //  Write a method isDateTimeBefore(LocalDateTime dateTime1, LocalDateTime dateTime2) that takes 
+  // two LocalDateTime objects and returns true if dateTime1 is before dateTime2, otherwise returns false.
+  public LocalDate getCurrentDateTime(){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    LocalDate currentDateTime = LocalDate.now();
+    return currentDateTime;
+  } 
+  public boolean isDateTimeBefore(LocalDateTime dateTime1,LocalDateTime dateTime2){
+     return dateTime1.isBefore(dateTime2);
+  }
+  public static void main(String[] args) {
+    LocalDateTimeExp lDateTimeExp = new LocalDateTimeExp();
+    LocalDate lDateTime = lDateTimeExp.getCurrentDateTime();
+    System.out.println("Current Date and Time in the Format of yyyy-MM-dd HH:mm:ss: "+lDateTime);
+    LocalDateTime dateTime1=LocalDateTime.of(2024, 8, 20, 10, 30);
+    LocalDateTime dateTime2 = LocalDateTime.of(2024, 8, 21, 15, 45);
+    boolean result =lDateTimeExp.isDateTimeBefore(dateTime1, dateTime2);
+    System.out.println("DateTime1 is before DateTime2: "+result);
+  }
+  }
+
